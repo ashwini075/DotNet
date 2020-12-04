f50037a4f1fad2432dd07e6748dfd0dfd0e614c8
ashwini075 <ashwinigaikar075@gmail.com>
2020-12-04 09:57:12 +0530

    Assignment 3

--- Assignment3/Employee/Program.cs
+++ Assignment3/Employee/Program.cs
@@ -10,19 +10,34 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            Manager o = new Manager("Ashwini",10000,2,"QualityManager");
-            Console.WriteLine( "Name:"+o.Name+"   Salary:"+o.CalNetSalary());
-
+            Manager o = new Manager("Ashwini", 10000, 2, "QualityManager");
+            Console.WriteLine("Name:" + o.Name + "   Salary:" + o.CalNetSalary());
+            o.Insert();
+            o.Update();
+            o.Delete();
+            Console.WriteLine();
             GeneralManager o1 = new GeneralManager("Ankita", 20000, 3, "PL", "health");
             Console.WriteLine("Name:" + o1.Name + "   Salary:" + o1.CalNetSalary());
-
-            CEO o2 = new CEO("Manvi", 25000, 4); 
+            o1.Insert();
+            o1.Update();
+            o1.Delete();
+            Console.WriteLine();
+            CEO o2 = new CEO("Manvi", 25000, 4);
             Console.WriteLine("Name:" + o2.Name + "   Salary:" + o2.CalNetSalary());
-
+            o2.Insert();
+            o2.Update();
+            o2.Delete();
             Console.ReadLine();
         }
     }
-    public abstract class Employee
+    public interface IDbFunctions
+    {
+        void Insert();
+        void Update();
+        void Delete();
+
+    }
+    public abstract class Employee:IDbFunctions
     {
         private string name;
         public string Name
@@ -36,7 +51,7 @@ namespace Employee
             }
             get
             { return name; }
-            
+
         }
         private int empNo;
         public int EmpNo
@@ -61,9 +76,24 @@ namespace Employee
         }
 
 
-    
+
         public abstract decimal CalNetSalary();
-       
+
+        public void Insert()
+        {
+            Console.WriteLine("Insert in Employee class");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Update in Employee class");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Delete in Employee class");
+        }
+
         static int id = 0;
 
         public Employee(string name = "Ash", decimal basic = 1000, short dept = 10)
@@ -77,7 +107,7 @@ namespace Employee
         }
 
     }
-    public class Manager : Employee
+    public class Manager : Employee, IDbFunctions
     {
         private string designation;
         public string Designation
@@ -103,14 +133,29 @@ namespace Employee
             decimal Sal = Basic + DA;
             return Sal;
         }
-        
-        public Manager(string name="None", decimal basic=1000,short dept=10,string designation="M"):base(name,basic,dept)
+
+      void IDbFunctions.Insert()
+        {
+            Console.WriteLine("Insert in Manager class");
+        }
+
+       public new void Update()
+        {
+            Console.WriteLine(" Update in Manager class");
+        }
+
+       public new void Delete()
+        {
+            Console.WriteLine("Delete in Manager class");
+        }
+
+        public Manager(string name = "Ash", decimal basic = 1000, short dept = 10, string designation = "M") : base(name, basic, dept)
         {
             this.Designation = designation;
 
         }
     }
-    public class GeneralManager : Manager
+    public class GeneralManager : Manager,IDbFunctions
     {
         private string perks;
         public string Perks
@@ -118,13 +163,29 @@ namespace Employee
             set { perks = value; }
             get { return perks; }
         }
-        public GeneralManager(string name="none", decimal basic=1000, short dept=10, string designation="GM",string perks="none"):base(name,basic,dept,designation)
+        public GeneralManager(string name = "Ash", decimal basic = 1000, short dept = 10, string designation = "GM", string perks = "none") : base(name, basic, dept, designation)
         {
             Console.WriteLine("In GeneralManger class");
             this.Perks = perks;
         }
+        public new void Insert()
+        {
+            Console.WriteLine("Insert in GManager class");
+        }
+
+        public new void Update()
+        {
+            Console.WriteLine("Update in GManagerclass");
+        }
+
+        public new void Delete()
+        {
+            Console.WriteLine("Delete in GManager class");
+        }
+
+       
     }
-    public class CEO : Employee
+    public class CEO : Employee,IDbFunctions
     {
 
         public override decimal Basic { set; get; }
@@ -132,14 +193,29 @@ namespace Employee
         public sealed override decimal CalNetSalary()
         {
             Console.WriteLine("CEO calnetsal");
-           decimal DA=500;
-            decimal Sal = Basic + DA ;
+            decimal DA = 500;
+            decimal Sal = Basic + DA;
             return Sal;
         }
-        public CEO(string name="None",decimal basic=1000,short dept=10):base(name,basic,dept)
+        public CEO(string name = "none", decimal basic = 1000, short dept = 10) : base(name, basic, dept)
         {
             Console.WriteLine("In CEO class");
         }
+        void IDbFunctions.Insert()
+        {
+            Console.WriteLine("Insert In CEO class");
+        }
 
+        public new void Update()
+        {
+            Console.WriteLine("Update In CEO class");
+        }
+
+        public new void Delete()
+        {
+            Console.WriteLine("Delete In CEO class");
+        }
+
+       
     }
 }
