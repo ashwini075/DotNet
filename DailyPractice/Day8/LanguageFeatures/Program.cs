ff69b3bf5b981ff0319ba88c5016c1e2ebd2ba8d
ashwini075 <ashwinigaikar075@gmail.com>
2020-12-08 22:47:08 +0530

    .

--- DailyPractice/Day8/LanguageFeatures/Program.cs
+++ DailyPractice/Day8/LanguageFeatures/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Implicite variables
 namespace LanguageFeatures
 {
     class Program
@@ -15,11 +15,43 @@ namespace LanguageFeatures
            // i2 = 200;
             Console.WriteLine(i2.GetType().ToString());
             Console.ReadLine();
-            SortedList<int, string> obj = new SortedList<int, string>();
-            foreach (var item in obj)
-            {
+            //SortedList<int, string> obj = new SortedList<int, string>();
+            //foreach (var item in obj)
+            //{
 
-            }
+            //}
+        }
+    }
+}
+
+namespace LanguageFeatures2
+{
+    public class Class1
+    {
+        public int i, j;
+        public Class1()
+        {
+            i = 100;
+            j = 200;
+        }
+        public Class1(int i,int j)
+        {
+            this.i = i;
+            this.j = j;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            Class1 o = new Class1();
+            o.i = 232;
+            o.j = 343;
+            Class1 o2 = new Class1() { i = 24, j = 54 };
+            Class1 o3 = new Class1() { i = 3,j=64 };
+
+            Class1 o4 = new Class1(34,23) { i = 23, j = 56 };
+
         }
     }
 }
@@ -27,7 +59,7 @@ namespace LanguageFeatures4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main4(string[] args)
         {
             int i=100;
             string s= "aa";
