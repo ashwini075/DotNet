7ff01821b5768991d1301507995f94ba7d8ae0ff
ashwini075 <ashwinigaikar075@gmail.com>
2020-12-15 00:15:29 +0530

    WpF practice

---
+++ DailyPractice/Day12/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            txtFullName.Text = txtFirstName.Text + " " + txtLastName.Text;
+        
+        }
+
+        private void txtFullName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("txtFirstName_keyDown" + " " + "txtLastName_keyDown");
+
+        }
+
+        private void txtFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+            txtFullName.Text = txtFirstName.Text + " " + txtLastName.Text;
+
+        }
+
+        private void txtFirstName_KeyDown(object sender, KeyEventArgs e)
+        {
+           // MessageBox.Show("txtFirstName_keyDown");
+        }
+
+        private void txtLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtFullName.Text = txtFirstName.Text + " " + txtLastName.Text;
+
+        }
+    }
+}
---
+++ DailyPractice/Day12/WpfApp1/Window2.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        static int n1;
+        static int n2;
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+             
+            int result= Int32.Parse(txtNum1.Text ) +  Int32.Parse(txtNum2.Text);
+            txtSum.Text = result.ToString();
+        }
+
+        private void txtNum1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            n1 = Int32.Parse(txtNum1.Text);
+            int sum = n1 + n2;
+            txtSum.Text = sum.ToString();
+        }
+
+        private void txtNum2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            n2 = Int32.Parse(txtNum2.Text);
+            int sum = n2 + n1;
+           txtSum.Text = sum.ToString();
+
+        }
+
+        private void txtSum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           int sum = n1 + n2;
+            txtSum.Text = sum.ToString();
+          
+        }
+    }
+}
